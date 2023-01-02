c612e80aa47cb1b85333a80f70106c718c942a17
Putt <57494734+PuttTim@users.noreply.github.com>
2023-01-03 04:29:33 +0700

    ✨ Created Jupiter's Create Thumbnail Lambda to generate pre-signed S3 urls

---
+++ jupiter-service/lambdas/jupiter-createThumbnail/index.ts
@@ -0,0 +1,39 @@
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+
+const REGION = "us-east-1"
+
+const s3Client = new S3Client({ region: REGION })
+
+export const handler = async (event) => {
+	// TODO implement
+	const response = {
+		statusCode: 200,
+		body: JSON.stringify("Hello from Lambda!"),
+	}
+
+	const key = event.pathParameters.modId
+
+	const signedUrl = await createSignedUrl(key)
+
+	return {
+		...response,
+		body: JSON.stringify(signedUrl),
+	}
+}
+
+const createSignedUrl = async (key) => {
+	const command = new PutObjectCommand({
+		Bucket: "orbit-image",
+		Key: `thumbnail/${key}/icon.png`,
+		ContentType: "image/png",
+	})
+
+	const signedUrl = await getSignedUrl(s3Client, command, {
+		expiresIn: 3600,
+	})
+
+	return signedUrl
+}
+
+console.log(await handler({ pathParameters: { modId: "211dd2608ae2" } }))
