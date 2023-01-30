c3542b94a5bbfa15e129954af6c9eb407c8a4645
Putt <57494734+PuttTim@users.noreply.github.com>
2023-01-31 06:30:47 +0800

    ⚡ Edited Vite config and formatted Theme.ts with a default home page now

--- website/src/pages/Home.tsx
+++ website/src/pages/Home.tsx
@@ -1,5 +1,28 @@
+import { Button, Container, Paper, Text } from "@mantine/core"
+
 const Home = () => {
-    return <div>Home</div>
+    return (
+        <div>
+            <Text>
+                Gravitate to 00 mods that
+                <Text span color="accent.9">
+                    {" "}
+                    Orbit{" "}
+                </Text>
+                our platform for your Minecraft World today!
+            </Text>
+            <Container size="md" bg="secondary.9">
+                HELLO
+            </Container>
+            <Paper p="md" bg="primary.9">
+                <Text>
+                    Gravitate to 00 mods that Orbit our platform for your
+                    Minecraft World today!
+                </Text>
+                <Button color="accent.2">Explore now!</Button>
+            </Paper>
+        </div>
+    )
 }
 
 export default Home
