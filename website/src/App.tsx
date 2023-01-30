c6c28926094883faee256f1716fc8307d8e741dc
PuttTim <yannaputt@gmail.com>
2023-01-30 10:58:01 +0800

    ⚡ Added colours to the MantineTheme

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -6,12 +6,20 @@ function App() {
 
 	return (
 		<div>
-			<Text>Gravitate to 00 mods that Orbit our platform for your Minecraft World today!</Text>
-			<Container size="md" color="accent.1">
+			<Text>
+				Gravitate to 00 mods that
+				<Text span color="accent.9">
+					{" "}
+					Orbit{" "}
+				</Text>
+				our platform for your Minecraft World today!
+			</Text>
+			<Container size="md" bg="secondary.9">
 				HELLO
 			</Container>
-			<Paper p="md" color="accent.1">
+			<Paper p="md" bg="primary.9">
 				<Text>Gravitate to 00 mods that Orbit our platform for your Minecraft World today!</Text>
+				<Button color="accent.2">Explore now!</Button>
 			</Paper>
 		</div>
 	)
