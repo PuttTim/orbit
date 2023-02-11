9df8a63ba99cbfc5383b310d8065d313b4afdf9e
PuttTim <yannaputt@gmail.com>
2023-02-11 12:55:35 +0800

    ⚡ Updated io-createS3URL to include file_name and version_id

--- io-service/lambdas/io-createS3URL/index.js
+++ io-service/lambdas/io-createS3URL/index.js
@@ -2,27 +2,33 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
 
 const REGION = "us-east-1"
+const headers = { "Content-Type": "application/json" }
 
 const s3Client = new S3Client({ region: REGION })
 
 export const handler = async (event) => {
-	const key = event.pathParameters.mod_id
+	const modId = event.pathParameters.mod_id
+	const fileName = event.pathParameters.file_name
+	const versionId = event.pathParameters.version_id
 
-	const signedUrl = await createSignedUrl(key)
+	const signedUrl = await createSignedUrl(modId, versionId, fileName)
 
-	console.log(key)
+	console.log(modId)
 	console.log(signedUrl)
 
 	return {
 		statusCode: 200,
-		body: JSON.stringify(signedUrl),
+		headers,
+		body: JSON.stringify({
+			url: signedUrl,
+		}),
 	}
 }
 
-const createSignedUrl = async (modId) => {
+const createSignedUrl = async (modId, versionId, fileName) => {
 	const command = new PutObjectCommand({
 		Bucket: "orbit-mods",
-		Key: `${modId}/`,
+		Key: `${modId}/${versionId}/${fileName}`,
 		ContentType: "binary/octet-stream",
 	})
 
